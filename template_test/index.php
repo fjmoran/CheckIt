bed37188c45145b2f7d436f240d3a3d07894ca14
Francisco Morán M <fjmoran@gmail.com>
2014-05-08 16:50:04 -0400

    mas diseño

--- template_test/index.php
+++ template_test/index.php
@@ -42,19 +42,25 @@
     padding-bottom: 12px;
   }
   .navbar-default {
-    background-color: #222 !important;
-    border-color: #222 !important;
+    background-color: #222;
+    border-color: #222;
   }
   .navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:hover, .navbar-default .navbar-nav>.active>a:focus {
     color: #ccc;
     background-color: #222;
+    border-left: 5px solid #d2322d;
   }
   .navbar-default .navbar-nav>li>a:hover, .navbar-default .navbar-nav>li>a:focus {
     color: #aaa;
     background-color: transparent;
+    border-left: 5px solid #d2322d;
   } 
+  .navbar-default .navbar-nav>li>a {
+    border-left: 5px solid #222;
+  }   
   .navbar-inverse .navbar-nav>.active>a, .navbar-inverse .navbar-nav>.active>a:hover, .navbar-inverse .navbar-nav>.active>a:focus {
     color: #ccc;
+    background-color: #222;
   } 
   .navbar-inverse .navbar-nav>li>a:hover, .navbar-inverse .navbar-nav>li>a:focus {
     color: #aaa;
@@ -93,9 +99,9 @@
         </div>
         <div class="collapse navbar-collapse pull-right">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#"><i class="icon-dashboard icon-large"></i> Dashboard</a></li>
-            <li><a href="#"><i class="icon-inbox icon-large"></i> <span class="badge badge-red">42</span> Procesos</a></li>
-            <li><a href="#"><i class="icon-briefcase icon-large"></i> Gestión Estratégica</a></li>
+            <li class="active"><a href="#"><i class="icon-dashboard icon-large icon-fixed-width"></i> Dashboard</a></li>
+            <li><a href="#"><i class="icon-inbox icon-large icon-fixed-width"></i> <span class="badge badge-red">42</span> Procesos</a></li>
+            <li><a href="#"><i class="icon-briefcase icon-large icon-fixed-width"></i> Gestión Estratégica</a></li>
           </ul>
         </div><!--/.nav-collapse -->
 
@@ -119,10 +125,10 @@
         </div>
         <div class="navbar-collapse collapse sidebar-navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Menu Item 1</a></li>
-            <li><a href="#">Menu Item 2</a></li>
-            <li><a href="#">Menu Item 3</a></li>
-            <li><a href="#">Menu Item 4</a></li>
+            <li class="active"><a href="#"><i class="icon-dashboard icon-large icon-fixed-width"></i> Dashboard</a></li>
+            <li><a href="#"><i class="icon-inbox icon-large icon-fixed-width"></i> Procesos<span class="badge badge-red pull-right">42</span></a></li>
+            <li><a href="#"><i class="icon-briefcase icon-large icon-fixed-width"></i> Gestión Estratégica</a></li>
+            <li><a href="#"><i class="icon-cogs icon-large icon-fixed-width"></i> Administación</a></li>
             <li><a href="#">Reviews <span class="badge pull-right badge-red">30</span></a></li>
           </ul>
         </div><!--/.nav-collapse -->
@@ -132,7 +138,7 @@
         </div>
         <div class="col-md-10 bg-colorlight">
           <div>
-            <h1>Aqui va el contenido</h1>
+            <h1>Aquí va el contenido</h1>
             <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>           
           </div>
@@ -141,7 +147,7 @@
 
     <div class="row" id="footer"> 
       <div class="col-md-12 bg-color">
-        <p class="text-muted credit text-center"> Un producto de: XXXXX.
+        <p class="text-muted credit text-center">Aquí va el footer.
         </p>
       </div>
     </div><!-- footer -->   
