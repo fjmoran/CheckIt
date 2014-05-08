bfb0abef8534faf4af5270dbaca2e441da77f00f
Francisco Morán M <fjmoran@gmail.com>
2014-05-08 09:53:07 -0400

    avance

---
+++ template_test/index.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="">
+
+    <title>Check!It</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../resources/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../resources/font-awesome/css/font-awesome.min.css">
+
+  </head>
+
+  <body style="padding-top: 50px;"> <!-- Mover estilos a un archivo checkit.css -->
+
+    <div class="navbar navbar-inverse navbar-fixed-top" style="border-color: #222;" role="navigation">
+
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Check!It</a>
+        </div>
+        <div class="collapse navbar-collapse pull-right">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#"><i class="icon-dashboard icon-large"></i> Dashboard</a></li>
+            <li><a href="#"><i class="icon-inbox icon-large"></i> Procesos</a></li>
+            <li><a href="#"><i class="icon-briefcase icon-large"></i> Gestión Estratégica</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+
+    </div>
+
+      <div class="row" id="contenido" style="background-color: #222;">
+        <div class="col-md-2">
+        <div style="color: #FFF;">Aqui va el menu</div>
+        </div>
+        <div class="col-md-10" style="background-color: #ddd;">
+          <div>
+            <h1>Aqui va el contenido</h1>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>           
+          </div>
+        </div>
+      </div> <!-- contenido -->
+
+    <div class="row" id="footer"> 
+      <div class="col-md-12" style="background-color: #222;">
+        <p class="text-muted credit text-center"> Un producto de: XXXXX.
+        </p>
+      </div>
+    </div><!-- footer -->   
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../resources/jquery/1.11.1/jquery.min.js"></script>
+    <script src="../resources/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
