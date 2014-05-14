c506800c8402b3d120e159068e75c12ac3cd8d03
Francisco Morán M <fjmoran@gmail.com>
2014-05-14 15:27:55 -0400

    mas footer

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -97,9 +97,9 @@
 
 		<div class="row" id="footer"> 
 			<div class="col-md-12 bg-color">
-				<p class="text-muted credit text-center pull-right" style="padding-top: 10px;">
-					<i class="fa fa-facebook"></i>
-					&nbsp&nbsp
+				<p class="text-muted credit pull-right" style="padding-top: 10px;">
+					<i class="fa fa-facebook-square"></i>
+					&nbsp
 					<i class="fa fa-twitter"></i>
 					 | Check!It 2014.</p>
 			</div>
