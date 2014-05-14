d0f8561289e25379fa83413dc591bfacb681a909
Francisco Morán M <fjmoran@gmail.com>
2014-05-14 14:38:50 -0400

    icono en usuario

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -21,11 +21,11 @@ if (Yii::app()->utility->isActiveMenu('admin')) {
 		),
 		'encodeLabel'=>false,
 		'items'=>array(
-			array('label'=>'<i class="fa fa-dashboard fa-lg fa-fw"></i> Usuarios', 'url'=>array('/user/admin'), 'active'=>Yii::app()->utility->isActiveSubMenu('usuarios')),
-
+			array('label'=>'<i class="fa fa-user fa-lg fa-fw"></i> Usuarios', 'url'=>array('/user/admin'), 'active'=>Yii::app()->utility->isActiveSubMenu('usuarios')),
+			/*
 			array('label'=>'<i class="fa fa-inbox fa-lg fa-fw"></i> Flujos de Proceso<span class="badge badge-red pull-right">42</span>', 'url'=>array('/site/page', 'view'=>'about'), 'visible'=>Yii::app()->user->checkAccess('process')),
 			array('label'=>'<i class="fa fa-briefcase fa-lg fa-fw"></i> Gestión Estratégica', 'url'=>array('/site/contact'), 'visible'=>Yii::app()->user->checkAccess('strategy')),
-			array('label'=>'<i class="fa fa-cogs fa-lg fa-fw"></i> Administración', 'url'=>array('/site/contact'), 'visible'=>Yii::app()->user->checkAccess('admin')),
+			array('label'=>'<i class="fa fa-cogs fa-lg fa-fw"></i> Administración', 'url'=>array('/site/contact'), 'visible'=>Yii::app()->user->checkAccess('admin')), */
 		),
 	)); 
 }?>
