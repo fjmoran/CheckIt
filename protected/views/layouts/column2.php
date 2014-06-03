f99353c335f49e6729973fe0140aaf7c3ffe1007
Francisco Morán M <fjmoran@gmail.com>
2014-06-03 11:25:18 -0400

    icono perspectivas

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -44,7 +44,7 @@ if (Yii::app()->utility->isActiveMenu('strategy')) {
 		),
 		'encodeLabel'=>false,
 		'items'=>array(
-			array('label'=>'<i class="fa fa-user fa-lg fa-fw"></i> Mis '.Yii::app()->utility->getOption('projects_name').'<span class="badge badge-red pull-right">'.$alert_tasks.'</span>', 'url'=>array('/project/myprojects'), 'active'=>Yii::app()->utility->isActiveSubMenu('myprojects')),
+			array('label'=>'<i class="fa fa-list-ul fa-lg fa-fw"></i> Mis '.Yii::app()->utility->getOption('projects_name').'<span class="badge badge-red pull-right">'.$alert_tasks.'</span>', 'url'=>array('/project/myprojects'), 'active'=>Yii::app()->utility->isActiveSubMenu('myprojects')),
 		),
 	)); 
 }?>
