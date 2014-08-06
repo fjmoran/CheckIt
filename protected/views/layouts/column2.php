9f89d596eefb7146148b6bd40cb8f42a7c9e3a83
Francisco Morán M <fjmoran@gmail.com>
2014-08-06 18:51:48 -0400

    por hacer --> mis tareas

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -48,7 +48,7 @@ if (Yii::app()->utility->isActiveMenu('strategy')) {
 		'items'=>array(
 			array('label'=>'<i class="fa fa-university fa-lg fa-fw"></i> Misión y Visión', 'url'=>array('/project/strategydata'), 'active'=>Yii::app()->utility->isActiveSubMenu('strategydata')),
 			array('label'=>'<i class="fa fa-list-ul fa-lg fa-fw"></i> Mis '.Yii::app()->utility->getOption('projects_name'), 'url'=>array('/project/myprojects'), 'active'=>Yii::app()->utility->isActiveSubMenu('myprojects')),
-			array('label'=>'<i class="fa fa-clock-o fa-lg fa-fw"></i> Por Hacer '.'<span class="badge badge-red pull-right">'.$alert_tasks.'</span>', 'url'=>array('/project/todo'), 'active'=>Yii::app()->utility->isActiveSubMenu('todo')),
+			array('label'=>'<i class="fa fa-clock-o fa-lg fa-fw"></i> Mis Tareas '.'<span class="badge badge-red pull-right">'.$alert_tasks.'</span>', 'url'=>array('/project/todo'), 'active'=>Yii::app()->utility->isActiveSubMenu('todo')),
 			array('label'=>'<i class="fa fa-check-square-o fa-lg fa-fw"></i> Terminados', 'url'=>array('/project/completed'), 'active'=>Yii::app()->utility->isActiveSubMenu('completed')),
 		),
 	)); 
