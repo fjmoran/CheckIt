93872e872661df31be150cedcd76aed7860e3850
Francisco Morán M <fjmoran@gmail.com>
2014-08-11 18:14:16 -0400

    css y saca comentarios

--- protected/views/project/report.php
+++ protected/views/project/report.php
@@ -19,9 +19,6 @@ foreach ($sps as $sp) {
 	<div class="col-md-11">
 		<h2><?php echo Yii::app()->utility->getOption('project_name');?>: <?php echo $model->name?></h2>
 	</div>
-	<!-- div class="col-md-1" style="padding-top: 20px;">
-		<a href="<?php echo Yii::app()->createUrl('site/report'); ?>" class="btn btn-primary btn-sm pull-right"><i class="fa fa-arrow-left"></i> Volver</a>
-	</div -->
 </div>
 <br>
 <div class="row">
@@ -55,79 +52,6 @@ foreach ($sps as $sp) {
 			'style'=>'width:250px; height:150px; margin: 0 auto;',
 		),
 	));?>
-
-	<?php /* $this->Widget('ext.highcharts.HighchartsWidget', array(
-		'scripts' => array(
-			'highcharts-more',   // enables supplementary chart types (gauge, arearange, columnrange, etc.)
-			'modules/solid-gauge',
-			//'modules/exporting', // adds Exporting button/menu to chart
-		),
-		'options'=>array(
-			'credits' => array('enabled' => false),
-			'chart' => array('type' => 'solidgauge'),
-			'title' => array('text' => $d['title']),
-			//'title' => null,
-			'pane' => array(
-				'center' => array('50%', '50%'),
-				'size' => '100%',
-				'startAngle' => -90,
-				'endAngle' => 90,
-				'background' => array(
-					'backgroundColor' => '#EEE',
-					'innerRadius' => '60%',
-					'outerRadius' => '100%',
-					'shape' => 'arc',
-				),
-			),
-			'tooltip' => array(
-				'enabled' => false,
-			),
-			'yAxis' => array(
-				'min' => 0,
-				'max' => 100,
-				'stops' => array(
-					array(0, '#c9302c'), //red
-					array($red - 0.001, '#c9302c'), //red
-					array($red, '#ffff33'), //yellow
-					array($yellow - 0.001, '#ffff33'), //yellow
-					array($yellow, '#449d44'), //green
-					array(1, '#449d44'), //green
-				),
-				'lineWidth' => 0,
-				'minorTickInterval' => 20,
-				'tickPixelInterval' => 100,
-				'tickWidth' => 0,
-				//'title' => array(
-				//	'y' => -70,
-				//	'text' => 'Speed',
-				//),
-				'labels' => array(
-					'y' => 16,
-				),
-			),
-			'plotOptions' => array(
-				'solidgauge' => array(
-					'dataLabels' => array(
-						'y' => 5,
-						'borderWidth' => 0,
-						'useHTML' => true,
-					),
-				),
-			),
-			'series' => array(
-				array(
-					'name' => 'Perspectiva',
-					'data' => array($d['compliance']),
-					'dataLabels' => array(
-						'format' => '<div style="text-align:center"><span style="font-size:25px;color:black;">{y} %</span></div>',
-					),
-					'tooltip' => array(
-						'valueSuffix' => ' %',
-					),
-				),
-			),
-		)
-	));*/	?>
 
 	<br>
 	<?php if ($subproject->kpis): ?>
