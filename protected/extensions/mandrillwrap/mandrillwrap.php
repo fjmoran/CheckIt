62aabb404b1aa6a2b823e54668deab6d2f168944
Christian Oviedo <christian.oviedo@gmail.com>
2014-09-02 20:05:02 -0400

    Correccion mandrill

--- protected/extensions/mandrillwrap/mandrillwrap.php
+++ protected/extensions/mandrillwrap/mandrillwrap.php
@@ -51,84 +51,94 @@ class mandrillwrap extends CApplicationComponent {
 
 		$ret = Mandrill::call((array) json_decode($request_json));*/
 
-		$mandrill_client = new Mandrill($this->mandrillKey);
-		$message = array(
-			"html" => $this->html,
-			"text" => strip_tags($this->html),
-			"subject" => $this->subject,
-			"from_email" => $this->fromEmail,
-			"from_name" => $this->fromName,
-			"to" => array(
-				array(
-					"email" => $this->toEmail,
-					"name" => $this->toName,
-					"type" => "to",
-				),
-			),
-			"headers" => array(
-				"Reply-To" => $this->fromEmail,
-			),
-			/*"important": false,
-			"track_opens": null,
-			"track_clicks": null,
-			"auto_text": null,
-			"auto_html": null,
-			"inline_css": null,
-			"url_strip_qs": null,
-			"preserve_recipients": null,
-			"view_content_link": null,
-			//"bcc_address": "message.bcc_address@example.com",
-			"tracking_domain": null,
-			"signing_domain": null,
-			"return_path_domain": null,
-			"merge": true,
-			"global_merge_vars": [{
-					"name": "merge1",
-					"content": "merge1 content"
-				}],
-			"merge_vars": [{
-					"rcpt": "recipient.email@example.com",
-					"vars": [{
-							"name": "merge2",
-							"content": "merge2 content"
-						}]
-				}],
-			"tags": [
-				"password-resets"
-			],
-			"subaccount": "customer-123",
-			"google_analytics_domains": [
-				"example.com"
-			],
-			"google_analytics_campaign": "message.from_email@example.com",
-			"metadata": {
-				"website": "app.getcheck.it"
-			},
-			"recipient_metadata": [{
-					"rcpt": "recipient.email@example.com",
-					"values": {
-						"user_id": 123456
-					}
-				}],
-			"attachments": [{
-					"type": "text/plain",
-					"name": "myfile.txt",
-					"content": "ZXhhbXBsZSBmaWxl"
-				}],
-			"images": [{
-					"type": "image/png",
-					"name": "IMAGECID",
-					"content": "ZXhhbXBsZSBmaWxl"
-				}]*/
-		);
-		$async = false;
-		//$ip_pool = "Main Pool";
-		$ip_pool = null;
-		//$send_at = "example send_at";
-		$send_at = null;
-		$result = $mandrill_client->messages->send($message, $async, $ip_pool, $send_at);
-		//print_r($result);
+		try {
 
+			$mandrill_client = new Mandrill($this->mandrillKey);
+			$message = array(
+				"html" => $this->html,
+				"text" => strip_tags($this->html),
+				"subject" => $this->subject,
+				"from_email" => $this->fromEmail,
+				"from_name" => $this->fromName,
+				"to" => array(
+					array(
+						"email" => $this->toEmail,
+						"name" => $this->toName,
+						"type" => "to",
+					),
+				),
+				"headers" => array(
+					"Reply-To" => $this->fromEmail,
+				),
+				/*"important": false,
+				"track_opens": null,
+				"track_clicks": null,
+				"auto_text": null,
+				"auto_html": null,
+				"inline_css": null,
+				"url_strip_qs": null,
+				"preserve_recipients": null,
+				"view_content_link": null,
+				//"bcc_address": "message.bcc_address@example.com",
+				"tracking_domain": null,
+				"signing_domain": null,
+				"return_path_domain": null,
+				"merge": true,
+				"global_merge_vars": [{
+						"name": "merge1",
+						"content": "merge1 content"
+					}],
+				"merge_vars": [{
+						"rcpt": "recipient.email@example.com",
+						"vars": [{
+								"name": "merge2",
+								"content": "merge2 content"
+							}]
+					}],
+				"tags": [
+					"password-resets"
+				],
+				"subaccount": "customer-123",
+				"google_analytics_domains": [
+					"example.com"
+				],
+				"google_analytics_campaign": "message.from_email@example.com",
+				"metadata": {
+					"website": "app.getcheck.it"
+				},
+				"recipient_metadata": [{
+						"rcpt": "recipient.email@example.com",
+						"values": {
+							"user_id": 123456
+						}
+					}],
+				"attachments": [{
+						"type": "text/plain",
+						"name": "myfile.txt",
+						"content": "ZXhhbXBsZSBmaWxl"
+					}],
+				"images": [{
+						"type": "image/png",
+						"name": "IMAGECID",
+						"content": "ZXhhbXBsZSBmaWxl"
+					}]*/
+			);
+			$async = false;
+			//$ip_pool = "Main Pool";
+			$ip_pool = null;
+			//$send_at = "example send_at";
+			$send_at = null;
+			$result = $mandrill_client->messages->send($message, $async, $ip_pool, $send_at);
+			//print_r($result);
+
+		} catch(Mandrill_Error $e) {
+
+			// Mandrill errors are thrown as exceptions
+			echo 'A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage();
+			// A mandrill error occurred: Mandrill_Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+			throw $e;
+
+		}
 
 	}
 }
